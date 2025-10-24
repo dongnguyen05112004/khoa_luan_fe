dc37ac2a774d4cc11a2a76df1e3337c287e53a7a
DongNguyen <dongnguyen05112004@gmail.com>
2025-10-24 13:35:37 +0700

    First commit - initial fe  project

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import Default from './layout/wrapper/index.vue'
+import Toaster from "@meforma/vue-toaster";
+
+const app = createApp(App)
+
+app.use(Toaster, {
+    position: "top-right",
+    duration: 3000,
+    max: 5
+})
+
+app.use(router)
+app.component("default-layout", Default);
+
+app.mount("#app")
---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from "vue-router"; // cài vue-router: npm install vue-router@next --save
+
+const routes = [
+    
+    {
+        path : '/admin/danhmuc',
+        component: ()=>import('../components/admin/danh_muc/danh_muc.vue')
+    },
+    
+    
+
+
+    
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: routes
+})
+
+export default router
