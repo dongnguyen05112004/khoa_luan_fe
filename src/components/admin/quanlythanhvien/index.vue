9aee49235a1893d872c6d048ee3aad1c241213fb
gnort00 <tronglehuu54@gmail.com>
2026-02-28 16:25:52 +0700

    trong git fee

---
+++ src/components/admin/quanlythanhvien/index.vue
@@ -0,0 +1,106 @@
+<template>
+    <div>
+        <div class="card">
+            <div class="row">
+                <div class="col-lg-8">
+                    <div class="col-lg-12 mt-3">
+                        <div class="row">
+                            <div class="col-lg-10">
+                                <div class="row">
+                                    <div class="iq-search-bar d-none d-md-block">
+                                        <form action="#" class="searchbox">
+                                            <input type="text" class="text search-input"
+                                                placeholder="Type here to search...">
+                                            <a class="search-link" href="#"><i class="ri-search-line"></i></a>
+
+                                        </form>
+                                    </div>
+
+                                    
+                                </div>
+
+
+                            </div>
+                            <div class="col-lg-2">
+                                <button class="btn btn-success text-center">
+                                    Add new member
+                                </button>
+                            </div>
+
+
+                        </div>
+
+
+
+                    </div>
+
+
+
+
+
+                    <div class="card rounded-4 border-0 shadow-sm p-4">
+
+                        <!-- Header -->
+                        <div class="d-flex justify-content-between align-items-center mb-3">
+                            
+                            <a href="#" class="text-primary small">
+                                View All &gt;
+                            </a>
+                        </div>
+
+                        <!-- Table -->
+                        <div class="table-responsive">
+                            <table class="table align-middle mb-0">
+                                <thead class="table-light rounded">
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Role</th>
+                                        <th>Specialization</th>
+                                        <th>Shift</th>
+                                        <th>Status</th>
+                                    </tr>
+                                </thead>
+
+                                <tbody>
+                                    <tr>
+                                        <td>hehe</td>
+                                        <td>PT</td>
+                                        <td>Body</td>
+                                        <td>Morning</td>
+                                        <td >Active</td>
+                                    </tr>
+                                    
+                                </tbody>
+                            </table>
+                        </div>
+
+                    </div>
+
+
+
+
+
+                </div>
+                <div class="col-lg-4">
+                     <div class="card">
+                        <h2>Member profile</h2>
+                        <h5>Name:..........</h5>
+                        <h5>Phone:..........</h5>
+                        <h5>Address:..........</h5>
+                        <h5>Assigned Clients:..........</h5>
+                        <h5>Monthly Commission:..........</h5>
+                        <h5>Attendance Rate:..........</h5>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style></style>
